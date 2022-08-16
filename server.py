254642d2183c7e840ebae406a229975dc3154015
iterekhov98@gmail.com <iterekhov98@gmail.com>
2022-08-16 22:26:48 +0300

    Initial commit

---
+++ server.py
@@ -0,0 +1,51 @@
+from aiohttp import web    
+import aiofiles
+import datetime
+import asyncio
+import os
+
+
+INTERVAL_SECS = 1
+
+
+async def handle_index_page(request):
+    async with aiofiles.open('index.html', mode='r') as index_file:
+        index_contents = await index_file.read()
+    return web.Response(text=index_contents, content_type='text/html')
+
+
+async def archive(request):
+    archive_hash = request.match_info.get('archive_hash')
+    archive_path = f'test_photos/{archive_hash}'
+    if not os.path.exists(archive_path):
+        raise web.HTTPNotFound(text='Архив не существует или был удален')
+
+    response = web.StreamResponse()
+    response.headers['Content-Type'] = 'text/html'
+    response.headers['Content-Disposition'] = 'attachment; filename="test_photos.zip"'
+
+    await response.prepare(request)
+
+    process = await asyncio.create_subprocess_exec('zip', '-', '-r', '.',
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+        cwd= f'test_photos/{archive_hash}'
+    )
+    while True:
+        if process.stdout.at_eof():
+            break    
+
+        archive_batch = await process.stdout.read(100*1024)
+        await response.write(archive_batch)
+
+    return response
+    
+
+
+if __name__ == '__main__':
+    app = web.Application()
+    app.add_routes([
+        web.get('/', handle_index_page),
+        web.get('/archive/{archive_hash}/', archive),
+    ])
+    web.run_app(app)
